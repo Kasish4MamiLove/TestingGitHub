d9b75e688395e1695d90612648e407f8ed00faee
Kasish4MamiLove <KassahunAmare4@gmail.com>
2017-02-01 04:05:47 -0500

    second commit

--- TestingGitHub/Form1.cs
+++ TestingGitHub/Form1.cs
@@ -20,6 +20,7 @@ namespace TestingGitHub
         private void Form1_Load(object sender, EventArgs e)
         {
             MessageBox.Show("Hello World!...Hello Tomorrow!");
+            MessageBox.Show("NvrStop Dreaming!");
         }
     }
 }
